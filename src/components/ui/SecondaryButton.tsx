da36a758ccbcab96a78363dc7850c07bae85c4e9
ryichi0 <ghazalmahmoodi23@gmail.com>
2026-09-06 17:04:35 +0330

    style: add pictures & restyle secondary button

--- src/components/ui/SecondaryButton.tsx
+++ src/components/ui/SecondaryButton.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default function SecondaryButton({title, link, className} : {title: string, link: string, className: string}) {
   return (
-    <Link className={`${className} text-foreground-secondary py-3 px-6 rounded-lg font-bold border border-border hover:shadow-card`} href={link}>{title}</Link>
+    <Link className={`${className} text-foreground-secondary py-3 px-6 rounded-lg font-bold border border-border hover:shadow-card text-center`} href={link}>{title}</Link>
   )
 }
